3e71fd3cbdff03f67a739e48846d332fcf498840
Rahul K Jha <jharahulkumar12@gmail.com>
2019-05-20 00:21:58 +0530

    Lec 158

---
+++ src/components/Burger/BuildControls/BuildControls.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class BuildControls extends Component {
+  render() {
+    return (
+      <div>
+        <p>sss</p>
+      </div>
+    );
+  }
+}
--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -4,11 +4,25 @@ import BurgerIngrediants from "./BurgerIngrediants/BuegerIngrediants";
 
 export default class Burger extends Component {
   render() {
+    //TODO Analyse this part of code - 8 to 14
+    let transformedIngrediants = Object.keys(this.props.ingrediants)
+      .map(igKey => {
+        return [...Array(this.props.ingrediants[igKey])].map((_, i) => {
+          return <BurgerIngrediants type={igKey} key={igKey + i} />;
+        });
+      })
+      .reduce((acc, curr) => {
+        return acc.concat(curr);
+      }, []);
+
+    if (transformedIngrediants.length === 0) {
+      transformedIngrediants = <p>Please start adding ingrediants</p>;
+    }
+
     return (
       <div className={classes.Burger}>
         <BurgerIngrediants type="bread-top" />
-        <BurgerIngrediants type="cheese" />
-        <BurgerIngrediants type="meat" />
+        {transformedIngrediants}
         <BurgerIngrediants type="bread-bottom" />
       </div>
     );
